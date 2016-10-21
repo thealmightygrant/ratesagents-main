5c2c147ec3294549c03efd9359a77fa83b9959c2
Grant Sherrick <agsherrick@gmail.com>
2016-10-20 18:42:19 -0600

    Progress on main sales page.

--- client_side/src/js/main.js
+++ client_side/src/js/main.js
@@ -7,5 +7,8 @@
     //init all popovers for now
     $('[data-toggle="popover"]').popover()
 
+    //init selects
+    $('select').material_select();
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
